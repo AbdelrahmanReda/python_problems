f5f91d7000fd18b09ada1d64f36f3c404ea46b48
Abdelrahman Reda Fadl <31633565+boodyds@users.noreply.github.com>
2018-02-16 18:02:01 +0200

    Update bakhshali_method.py

--- bakhshali_method.py
+++ bakhshali_method.py
@@ -1,11 +1,7 @@
 while True :
-    x= int(input())
-    
-    intailaizer=1
-    
-    
-    for q in range (len(str(x))+2):    
-        
+    x= int(input())    
+    intailaizer=1        
+    for q in range (len(str(x))+2):           
         comprator=None
         stage_one=(x-(intailaizer)**2)/(2*intailaizer)
         stage_three=stage_two-((stage_one)**2/(2*stage_two))
